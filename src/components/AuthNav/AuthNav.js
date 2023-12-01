75b1e0b23cbd25ca649aa5cf46ef266ed5026c37
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-12-01 17:12:03 +0300

    24

--- src/components/AuthNav/AuthNav.js
+++ src/components/AuthNav/AuthNav.js
@@ -1,11 +1,12 @@
 import { NavLink } from 'react-router-dom';
+import css from '../Navigation/Navigation.module.css'
 export const AuthNav = () => {
   return (
     <div>
-      <NavLink  to="/register">
+      <NavLink className={css.link} to="/register">
         Register
       </NavLink>
-      <NavLink  to="/login">
+      <NavLink className={css.link} to="/login">
         Log In
       </NavLink>
     </div>
--- src/components/UserMenu/UserMenu.js
+++ src/components/UserMenu/UserMenu.js
@@ -10,7 +10,7 @@ export const UserMenu = () => {
   return (
     <div className={css.wrapper}>
       <p className={css.username}>Welcome {user.name}!</p>
-      <button type="button" onClick={() => dispatch(logOut())}>
+      <button className={css.button} type="button" onClick={() => dispatch(logOut())}>
         Logout
       </button>
     </div>
