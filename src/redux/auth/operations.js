35f640a65bfe6f2903e340ddad61128b5ae2c794
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-12-02 12:37:47 +0300

    77

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -67,38 +67,6 @@ export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
  * GET @ /users/current
  * headers: Authorization: Bearer token
  */
-// export const refreshUser = createAsyncThunk(
-//   'auth/refresh',
-//   async (_, thunkAPI) => {
-//     // Reading the token from the state via getState()
-//     const state = thunkAPI.getState();
-//     const persistedToken = state.auth.token;
-
-//     if (persistedToken === null) {
-//       // If there is no token, exit without performing any request
-//       return thunkAPI.rejectWithValue('Unable to fetch user');
-//     }
-
-//     try {
-//       // If there is a token, add it to the HTTP header and perform the request
-//       setAuthHeader(persistedToken);
-//       const res = await axios.get('/users/me');
-//       return res.data;
-//     } catch (error) {
-//       return thunkAPI.rejectWithValue(error.message);
-//     }
-//   },
-// {
-//   condition: (_, thunkAPI) => {
-//     const state = thunkAPI.getState();
-//     const persistedToken = state.auth.token;
-//     if(!persistedToken ) 
-//       return false;
-//     return true;
-//   },
-// }
-// );
-
 
 export const refreshUser = createAsyncThunk(
   'auth/refresh',
