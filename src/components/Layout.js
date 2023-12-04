77970a4d1d93d3fc32ff66421b79e9d662e1f120
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-12-04 18:29:49 +0300

    123

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -5,7 +5,7 @@ import { Suspense } from 'react';
 
 export const Layout = () => {
   return (
-    <div style={{ margin: '0 auto', padding: '0 16px' }}>
+    <div style={{ margin: '0 auto', padding: '0 22px' }}>
       <AppBar />
       <Suspense fallback={null}>
         <Outlet />
