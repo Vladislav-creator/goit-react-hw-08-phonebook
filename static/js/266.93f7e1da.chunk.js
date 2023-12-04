7e846f0d64f00f48a21202afaa403aeafdea325a
Vladislav-creator <130439019+Vladislav-creator@users.noreply.github.com>
2023-12-04 16:23:47 +0000

    Deploying to gh-pages from @ Vladislav-creator/goit-react-hw-08-phonebook@62adc4336e4c441790ee659e754657d84a07adaa 🚀

---
+++ static/js/266.93f7e1da.chunk.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkgoit_react_hw_08_phonebook=self.webpackChunkgoit_react_hw_08_phonebook||[]).push([[266],{1266:function(e,t,n){n.r(t),n.d(t,{default:function(){return c}});var i=n(184),o={container:{minHeight:"calc(100vh - 50px)",display:"flex"},title:{fontWeight:500,fontSize:48,textAlign:"center",color:"white"}};function c(){return(0,i.jsx)("div",{style:o.container,children:(0,i.jsx)("h1",{style:o.title,children:"Contacts manager welcome page !!!"})})}}}]);
+//# sourceMappingURL=266.93f7e1da.chunk.js.map
